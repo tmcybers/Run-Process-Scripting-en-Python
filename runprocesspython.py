9caedf5ecac6d334242458c33c081613f1e1599f
TM Cyber <97669969+tmcyberagent@users.noreply.github.com>
2022-02-04 16:27:09 +0100

    Update runprocesspython.py

--- runprocesspython.py
+++ runprocesspython.py
@@ -1,5 +1,8 @@
+#Anton Merisan(TmCyber)
+#CyberSecurity | Python | Artificial Inteligence
+
 import subprocess
 
 subprocess.call("ifconfig", shell=True)
 
-
+print("We have opened our console to see our ip, in the same way we can open any process in our system...!")
